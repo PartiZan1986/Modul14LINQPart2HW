1f14bb6b60f29daf35259fa0693ed9dd535e931f
Yaroslav <kay3@yandex.ru>
2024-01-19 17:48:01 +0300

    Add project files.

---
+++ Modul14LINQPart2HW/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul14LINQPart2HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        private static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(x => x.Students).ToArray();
+        }
+    }
+}
